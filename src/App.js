dfc889b94b3d07f356ba11a63af97d328e196094
yanakiv30 <138647535+yanakiv30@users.noreply.github.com>
2024-01-04 15:08:12 +0200

    balance

--- src/App.js
+++ src/App.js
@@ -31,10 +31,36 @@ export default function App() {
 function FriendList() {
   const friends = initialFriends;
   return (
-    <Friend friends={friends}/>    
+    <ul>
+      {friends.map((friend) => (
+        <Friend friend={friend} key={friend.id} />
+      ))}
+    </ul>
   );
 }
 
-function Friend({friends}) {
-return friends.map(friend=> <li>{friend.name}</li>)
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} {Math.abs(friend.balance)}$
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+        {friend.name} owes you {Math.abs(friend.balance)}$
+        </p>
+      )}
+
+      {friend.balance === 0 && (
+        <p>
+          You and {friend.name} are even
+        </p>
+      )}
+    </li>
+  );
 }
